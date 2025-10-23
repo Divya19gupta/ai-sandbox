6ecbba44cdebbb60f4878d7143bd84fd30971a75
Divya19Gupta <divya2207.work@gmail.com>
2025-10-23 19:43:07 +0200

    completed looping and numbers

---
+++ python-course/problems/findevennumbs.py
@@ -0,0 +1,6 @@
+for i in range(0,10,2):
+    print(i)
+
+for i in range(0,10):
+    if i%2 is 0:
+        print(i)
---
+++ python-course/problems/loop.py
@@ -0,0 +1,15 @@
+'''
+start - 1 (i=1) compulsory
+stop - 10 (i<n) compulsory
+step - 1 (i++) optional
+
+
+break, continue
+pass: just act as a placeholder
+'''
+print(tuple(range(1,10,2)))
+
+for i in range(1,3):
+    for j in range(3,6):
+        print(i,j)
+
