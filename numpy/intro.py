98ddac876313db8eac7a4616466d4ee238d00905
Divya19Gupta <divya2207.work@gmail.com>
2025-11-03 19:03:07 +0100

    refactored and added pandas

---
+++ numpy/intro.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Age': [24, 27, 22, 32],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+}   
+df = pd.DataFrame(data)     
+print(df)
+
+'''
+# Data Analysis: It means you are using pandas to analyze tabular data.
+# Data Manipulation: You are creating and manipulating data
+
+#Encoding errors: You are using pandas to handle data that may have encoding issues and to resolve it use utf-8 or latin1
+#Series: A one-dimensional array-like object in pandas that can hold any data type.
+'''
