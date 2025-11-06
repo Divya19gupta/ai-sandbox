5ee563af99690d63dfc569f50e50e2cf4428fbe0
Divya19Gupta <divya2207.work@gmail.com>
2025-11-06 16:31:04 +0100

    added more topics

--- numpy/intro.py
+++ numpy/intro.py
@@ -3,7 +3,7 @@ print("NumPy imported successfully!")
 
 
 ## INTRO:
-
+print('Intro to NumPy')
 print(np.array([1, 2, 3]))
 print(np.zeros((2, 3))) #(2,3) shape of array, it fills the array with zeros as default value
 print(np.ones((3, 2))) #(3,2) shape of array, it fills the array with ones as default value
@@ -12,7 +12,7 @@ print(np.arange(0, 10, 2)) #Creates an array with values from 0 to 10 with a ste
 print(np.eye(3)) #Creates a 3x3 identity matrix where one is on the diagonal and zeros elsewhere
 
 ## ARRAY PROPERTIES AND OPERATIONS:
-
+print('Array Properties')
 #Shape? Size? Type?
 arr = np.array([[1, 2, 3], [4, 5, 6]])
 print(arr.shape) #Returns a tuple representing the dimensions of the array (rows * columns)
@@ -23,6 +23,7 @@ print(arr.astype(np.str_)) #Converts the array to a different data type (here, t
 #types: int, str, float64
 
 #Operations:
+print('Array Operations')
 print(arr + 10) #Adds 10 to each element in the array
 print(arr * 2) #Multiplies each element in the array by 2
 print(np.sqrt(arr)) #Calculates the square root of each element in the array
@@ -38,6 +39,25 @@ print(np.var(arr)) #Calculates the variance of the elements in the array
 #Indexing and Slicing: Picking single elements or sub-arrays vs selecting multiple elements or sub-arrays
 #Fancy Indexing: Using arrays of indices to select multiple elements at once
 #Boolean Masking: Using boolean arrays to filter elements based on conditions
+
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+print('Array Indexing and Slicing')
+print(arr[0, 1]) #Accesses the element at row 0, column 1
+print(arr[:, 1]) #Accesses all rows in column 1
+print(arr[1, :]) #Accesses all columns in row 1
+# [start:stop (counts one before ):step]
+print(arr[0:2, 1:3]) #Accesses a sub-array from row 0 to 1 and column 1 to 2
+print(arr[-1])
+
+print('Fancy Indexing') #helps in selecting non-sequential elements and do so by creating a copy of the data and does not change in the original array
+print(arr[[0, 1], [1, 2]]) #Accesses elements at (0, 1) and (1, 2)
+
+print('Boolean Masking/Filtering')
+
+
+
+
+
 
 
 ## DIFF BTW NUMPY AND PANDAS:
